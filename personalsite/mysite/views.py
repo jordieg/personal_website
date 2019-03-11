52baa4fe310321235c513cdeccd33925f5bee777
Jordan Graves <jordangraves@Jordans-MacBook-Pro.local>
2019-03-11 17:43:45 -0400

    fixing views

--- personalsite/mysite/views.py
+++ personalsite/mysite/views.py
@@ -1,6 +1,6 @@
 import json
 
-#import requests
+import requests
 from django.shortcuts import render
 
 from .models import Contact
@@ -11,8 +11,21 @@ def index(request):
         firstname = request.POST.get('fname')
         lastname = request.POST.get('lname')
 
+        r = requests.get('http://api.icndb.com/jokes/random?firstName=' + firstname + '&lastName=' + lastname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+
+        context = {'joker': joke}
         return render(request, 'mysite/index.html', context)
     else:
+        firstname = 'Attreya'
+        lastname = 'Bhatt'
+
+        r = requests.get('http://api.icndb.com/jokes/random?firstName=' + firstname + '&lastName=' + lastname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+
+        context = {'joker': joke}
         return render(request, 'mysite/index.html', context)
 
 
@@ -32,3 +45,41 @@ def contact(request):
         return render(request, 'mysite/thank.html')
     else:
         return render(request, 'mysite/contact.html')
+
+
+
+
+# import json
+
+# #import requests
+# from django.shortcuts import render
+
+# from .models import Contact
+
+
+# def index(request):
+#     if request.method == 'POST':
+#         firstname = request.POST.get('fname')
+#         lastname = request.POST.get('lname')
+
+#         return render(request, 'mysite/index.html', context)
+#     else:
+#         return render(request, 'mysite/index.html', context)
+
+
+# def portfolio(request):
+#     return render(request, 'mysite/portfolio.html')
+
+
+# def contact(request):
+#     if request.method == 'POST':
+#         email_r = request.POST.get('email')
+#         subject_r = request.POST.get('subject')
+#         message_r = request.POST.get('message')
+
+#         c = Contact(email=email_r, subject=subject_r, message=message_r)
+#         c.save()
+
+#         return render(request, 'mysite/thank.html')
+#     else:
+#         return render(request, 'mysite/contact.html')
